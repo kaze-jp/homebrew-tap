469061405f0bfc2e86ab02966923b6b313c3025f
Jin Nakamura <jin.nakamura@kaze.jp>
2026-03-12 11:49:31 +0900

    feat: add kusa cask formula

---
+++ Casks/kusa.rb
@@ -0,0 +1,34 @@
+cask "kusa" do
+  arch arm: "aarch64", intel: "x64"
+
+  version "0.1.0"
+
+  sha256 arm:   "REPLACE_WITH_ARM64_SHA256",
+         intel: "REPLACE_WITH_X86_64_SHA256"
+
+  url "https://github.com/kaze-jp/kusa/releases/download/v#{version}/kusa_#{version}_#{arch}.dmg",
+      verified: "github.com/kaze-jp/kusa/"
+
+  name "kusa"
+  desc "Markdown editor for AI developers"
+  homepage "https://github.com/kaze-jp/kusa"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :catalina"
+
+  app "kusa.app"
+
+  # Symlink the CLI binary so `kusa` is available in PATH
+  binary "#{appdir}/kusa.app/Contents/MacOS/kusa"
+
+  zap trash: [
+    "~/Library/Application Support/jp.kaze.kusa",
+    "~/Library/Caches/jp.kaze.kusa",
+    "~/Library/Preferences/jp.kaze.kusa.plist",
+    "~/Library/Saved Application State/jp.kaze.kusa.savedState",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,9 @@
+# homebrew-tap
+
+Homebrew tap for [kusa](https://github.com/kaze-jp/kusa).
+
+## Install
+
+```sh
+brew install kaze-jp/tap/kusa
+```
